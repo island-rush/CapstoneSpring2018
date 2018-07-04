74583aa18f81bb41680f80dbf5a086feddb7332f
spenceradolph <c19spencer.adolph@usafa.edu>
2018-07-03 20:30:20 -0600

    added ajax stuff, about to try and make 2 people connect to the server at the same time and they update with shit...makes sense to me i think

---
+++ event_handler.php
@@ -0,0 +1,17 @@
+<?php
+
+//$q = $_REQUEST["q"];
+
+
+
+
+
+
+
+
+
+
+
+
+//Whatever is echo'd is like the return from a function
+echo "THIS WAS ECHO FROM EVENT HANDLER";
---
+++ new_testing_page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <title>Login to Island Rush</title>
+    <link rel="stylesheet" type="text/css" href="style_welcome.css">
+    <script>
+        function cool_function() {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    document.getElementById("coolnew_id").innerHTML = this.responseText;
+                }
+            };
+            xmlhttp.open("GET", "event_handler.php", true);
+            xmlhttp.send();
+        }
+    </script>
+</head>
+
+<body>
+<h1>Island Rush</h1>
+<nav>
+    <a href="./home.html">Home</a>
+    <a class="active" href="./game_login.php">Play the Game</a>
+    <a href="new_testing_page.php">Testing Spencer's Shit</a>
+    <a style="float:right" href="./admin_login.php">Admin</a>
+</nav>
+
+<button onclick="cool_function()"><span id="coolnew_id">DEFAULT VALUE</span></button>
+
+
+</body>
+
+</html>
+
+
