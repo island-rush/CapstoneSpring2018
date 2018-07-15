1711f0d7beed8c58558e42d89332397a4024d7b0
spenceradolph <c19spencer.adolph@usafa.edu>
2018-07-15 14:31:30 -0600

    added all the pieces to the database, as well as to the css (colors for now, images later) (colors are easier to test with (and see on the map))

--- final_database.sql
+++ final_database.sql
@@ -56,11 +56,21 @@ CREATE TABLE IF NOT EXISTS `units`(
 
 -- Insert units into table.
 INSERT INTO `units` VALUES (1, 'transport', 5, 0, 2);
-INSERT INTO `units` VALUES (2, 'aircraftCarrier', 5, 1, 3);
+INSERT INTO `units` VALUES (2, 'submarine', 5, 1, 3);
 INSERT INTO `units` VALUES (3, 'destroyer', 5, 3, 4);
-INSERT INTO `units` VALUES (4, 'submarine', 5, 4, 4);
+INSERT INTO `units` VALUES (4, 'aircraftCarrier', 5, 4, 4);
 INSERT INTO `units` VALUES (5, 'soldier', 5, 2, 2);
-INSERT INTO `units` VALUES (6, 'test_piece', 5, 2, 2);
+INSERT INTO `units` VALUES (6, 'artillery', 5, 2, 2);
+INSERT INTO `units` VALUES (7, 'tank', 5, 2, 2);
+INSERT INTO `units` VALUES (8, 'marine', 5, 2, 2);
+INSERT INTO `units` VALUES (9, 'lav', 5, 2, 2);
+INSERT INTO `units` VALUES (10, 'attackHeli', 5, 2, 2);
+INSERT INTO `units` VALUES (11, 'sam', 5, 2, 2);
+INSERT INTO `units` VALUES (12, 'fighter', 5, 2, 2);
+INSERT INTO `units` VALUES (13, 'bomber', 5, 2, 2);
+INSERT INTO `units` VALUES (14, 'stealthBomber', 5, 2, 2);
+INSERT INTO `units` VALUES (15, 'tanker', 5, 2, 2);
+INSERT INTO `units` VALUES (16, 'test_piece', 5, 2, 2);
 
 
 -- Table for Game Board Allowed Positions (for troops and whatnot)
@@ -126,7 +136,6 @@ INSERT INTO `positions` VALUES (52, 'water');
 INSERT INTO `positions` VALUES (53, 'water');
 INSERT INTO `positions` VALUES (54, 'water');
 INSERT INTO `positions` VALUES (55, 'water');
--- 10 for big left? (missing the G)
 INSERT INTO `positions` VALUES (56, 'special_island13');
 INSERT INTO `positions` VALUES (57, 'special_island13');
 INSERT INTO `positions` VALUES (58, 'special_island13');
@@ -137,7 +146,6 @@ INSERT INTO `positions` VALUES (62, 'special_island13');
 INSERT INTO `positions` VALUES (63, 'special_island13');
 INSERT INTO `positions` VALUES (64, 'special_island13');
 INSERT INTO `positions` VALUES (65, 'special_island13');
--- 10 for big right
 INSERT INTO `positions` VALUES (66, 'special_island14');
 INSERT INTO `positions` VALUES (67, 'special_island14');
 INSERT INTO `positions` VALUES (68, 'special_island14');
@@ -148,62 +156,49 @@ INSERT INTO `positions` VALUES (72, 'special_island14');
 INSERT INTO `positions` VALUES (73, 'special_island14');
 INSERT INTO `positions` VALUES (74, 'special_island14');
 INSERT INTO `positions` VALUES (75, 'special_island14');
--- 4 for island1
 INSERT INTO `positions` VALUES (76, 'special_island1');
 INSERT INTO `positions` VALUES (77, 'special_island1');
 INSERT INTO `positions` VALUES (78, 'special_island1');
 INSERT INTO `positions` VALUES (79, 'special_island1');
--- 4 for island2
 INSERT INTO `positions` VALUES (80, 'special_island2');
 INSERT INTO `positions` VALUES (81, 'special_island2');
 INSERT INTO `positions` VALUES (82, 'special_island2');
 INSERT INTO `positions` VALUES (83, 'special_island2');
--- 3 for island3
 INSERT INTO `positions` VALUES (84, 'special_island3');
 INSERT INTO `positions` VALUES (85, 'special_island3');
 INSERT INTO `positions` VALUES (86, 'special_island3');
--- 4 for island4
 INSERT INTO `positions` VALUES (87, 'special_island4');
 INSERT INTO `positions` VALUES (88, 'special_island4');
 INSERT INTO `positions` VALUES (89, 'special_island4');
 INSERT INTO `positions` VALUES (90, 'special_island4');
--- 4 for island5
 INSERT INTO `positions` VALUES (91, 'special_island5');
 INSERT INTO `positions` VALUES (92, 'special_island5');
 INSERT INTO `positions` VALUES (93, 'special_island5');
 INSERT INTO `positions` VALUES (94, 'special_island5');
--- 3 for island6
 INSERT INTO `positions` VALUES (95, 'special_island6');
 INSERT INTO `positions` VALUES (96, 'special_island6');
 INSERT INTO `positions` VALUES (97, 'special_island6');
--- 3 for island7
 INSERT INTO `positions` VALUES (98, 'special_island7');
 INSERT INTO `positions` VALUES (99, 'special_island7');
 INSERT INTO `positions` VALUES (100, 'special_island7');
--- 3 for island8
 INSERT INTO `positions` VALUES (101, 'special_island8');
 INSERT INTO `positions` VALUES (102, 'special_island8');
 INSERT INTO `positions` VALUES (103, 'special_island8');
--- 4 for island9
 INSERT INTO `positions` VALUES (104, 'special_island9');
 INSERT INTO `positions` VALUES (105, 'special_island9');
 INSERT INTO `positions` VALUES (106, 'special_island9');
 INSERT INTO `positions` VALUES (107, 'special_island9');
--- 4 for island10
 INSERT INTO `positions` VALUES (108, 'special_island10');
 INSERT INTO `positions` VALUES (109, 'special_island10');
 INSERT INTO `positions` VALUES (110, 'special_island10');
 INSERT INTO `positions` VALUES (111, 'special_island10');
--- 3 for island11
 INSERT INTO `positions` VALUES (112, 'special_island11');
 INSERT INTO `positions` VALUES (113, 'special_island11');
 INSERT INTO `positions` VALUES (114, 'special_island11');
--- 4 for island12
 INSERT INTO `positions` VALUES (115, 'special_island12');
 INSERT INTO `positions` VALUES (116, 'special_island12');
 INSERT INTO `positions` VALUES (117, 'special_island12');
 INSERT INTO `positions` VALUES (118, 'special_island12');
-
 INSERT INTO `positions` VALUES (119, 'purchase_square');
 
 
@@ -219,26 +214,26 @@ CREATE TABLE IF NOT EXISTS `placements`(
     PRIMARY KEY(`placementId`),
     FOREIGN KEY (unitId) REFERENCES units(unitId),
     FOREIGN KEY (gameId) REFERENCES games(gameId),
-    FOREIGN KEY (transportId) REFERENCES placements(placementId),
     FOREIGN KEY (positionId) REFERENCES positions(positionId)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 -- Insert placements into table.
-INSERT INTO `placements` VALUES (1, 1, 6, 'red', 999999, 5, 1);
--- INSERT INTO `placements` VALUES (2, 1, 1, 'red', null, 5, 2);
--- INSERT INTO `placements` VALUES (3, 1, 2, 'red', 10, 3);
--- INSERT INTO `placements` VALUES (4, 1, 3, 'red', 10, 4);
--- INSERT INTO `placements` VALUES (5, 1, 4, 'red', 10, 5);
--- INSERT INTO `placements` VALUES (6, 1, 5, 'red', 10, 6);
-
--- INSERT INTO `placements` VALUES (2, 1, 6, 'red', 1);
--- INSERT INTO `placements` VALUES (3, 1, 6, 'red', 1);
--- INSERT INTO `placements` VALUES (4, 1, 6, 'red', 1);
-
--- INSERT INTO `placements` VALUES (5, 1, 6, 'red', 119);
-
--- this shows overflow mechanics (may change to 9 per position possible)
--- INSERT INTO `placements` VALUES (5, 1, 6, 'red', 1);
+INSERT INTO `placements` VALUES (1, 1, 1, 'red', 999999, 5, 1);
+INSERT INTO `placements` VALUES (2, 1, 2, 'red', 999999, 5, 2);
+INSERT INTO `placements` VALUES (3, 1, 3, 'red', 999999, 5, 3);
+INSERT INTO `placements` VALUES (4, 1, 4, 'red', 999999, 5, 4);
+INSERT INTO `placements` VALUES (5, 1, 5, 'red', 999999, 5, 5);
+INSERT INTO `placements` VALUES (6, 1, 6, 'red', 999999, 5, 6);
+INSERT INTO `placements` VALUES (7, 1, 7, 'red', 999999, 5, 7);
+INSERT INTO `placements` VALUES (8, 1, 8, 'red', 999999, 5, 8);
+INSERT INTO `placements` VALUES (9, 1, 9, 'red', 999999, 5, 9);
+INSERT INTO `placements` VALUES (10, 1, 10, 'red', 999999, 5, 10);
+INSERT INTO `placements` VALUES (11, 1, 11, 'red', 999999, 5, 11);
+INSERT INTO `placements` VALUES (12, 1, 12, 'red', 999999, 5, 12);
+INSERT INTO `placements` VALUES (13, 1, 13, 'red', 999999, 5, 13);
+INSERT INTO `placements` VALUES (14, 1, 14, 'red', 999999, 5, 14);
+INSERT INTO `placements` VALUES (15, 1, 15, 'red', 999999, 5, 15);
+INSERT INTO `placements` VALUES (16, 1, 16, 'red', 999999, 5, 16);
 
 
 -- Table of Movements
@@ -246,20 +241,19 @@ CREATE TABLE IF NOT EXISTS `movements`(
 	`movementId` int(16) NOT NULL AUTO_INCREMENT,
     `movementFromPosition` int(4) NOT NULL,  -- meat of what is used
     `movementNowPlacement` int(16) NOT NULL,  -- used for undo moves
-    `movementFromTransport` int(3),
+    `movementFromTransport` int(16),
     `movementCost` int(3) NOT NULL,
     `movementGameId` int(5) NOT NULL,  -- which game is the movement
     `movementTurn` int(5) NOT NULL,  -- need what phase/turn movement was made (only undo current phase/turn)
     `movementPhase` varchar(20) NOT NULL,
     PRIMARY KEY(`movementId`),
     FOREIGN KEY (movementGameId) REFERENCES games(gameId),
-    FOREIGN KEY (movementFromTransport) REFERENCES placements(placementId),
     FOREIGN KEY (movementFromPosition) REFERENCES positions(positionId),
     FOREIGN KEY (movementNowPlacement) REFERENCES placements(placementId)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 
--- this used for easy testing alone
+-- this used for easy testing alone (not sure if both teams must be logged in to play (watch)
 UPDATE games SET gameBlueJoined=1 WHERE gameId = 1;
 -- INSERT INTO placements (gameId, unitId, teamId, positionId) VALUES(1, 6, 'red', 119);
 
